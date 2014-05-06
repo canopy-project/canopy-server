2001ad2a3c29957fab9e082b6fd3555679cd419b
Greg Prisament <gregjp@toruslabs.com>
2014-05-06 02:01:08 -0700

    Implement echo websocket server in go

--- src/canopy.go
+++ src/canopy.go
@@ -2,14 +2,24 @@ package main
 
 import (
     "fmt"
+    "io"
     "net/http"
+    
+    "code.google.com/p/go.net/websocket"
 )
+
+// Echo the data received on the WebSocket
+func EchoServer(ws *websocket.Conn) {
+    io.Copy(ws, ws)
+}
+
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
+    http.Handle("/echo", websocket.Handler(EchoServer))
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
