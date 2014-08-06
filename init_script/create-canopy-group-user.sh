23ec6bbb776ab86a0c1fe9f52beef2edec0efbda
Greg Prisament <greg@canopy.link>
2014-08-05 20:57:34 -0700

    "make install" installs init scripts and creates users

--- init_script/create-canopy-group-user.sh
+++ init_script/create-canopy-group-user.sh
@@ -1,2 +1,2 @@
-addgroup --system canopy
-adduser --system --ingroup canopy --no-create-home --disabled-password canopy
+id -g canopy >> /dev/null || addgroup --system canopy
+id -u canopy >> /dev/null || adduser --system --ingroup canopy --no-create-home --disabled-password canopy
