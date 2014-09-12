fbcaca3da0445805c52e7d8c6a42f35876661dde
Greg Prisament <gregjp@toruslabs.com>
2014-09-12 16:08:31 -0700

    Prefix api endpoints with /api

--- src/canopy/rest/canopy_rest_api.go
+++ src/canopy/rest/canopy_rest_api.go
@@ -23,22 +23,17 @@ import (
 
 func GetRestHandler() http.Handler {
     r := mux.NewRouter()
-    /* TODO: add "/api" to the front of each of these.
-     * (Will need to update application.)
-     * 
-     * Doing this will minimize collisions when we start to allow /USERNAME/DEVICE
-     */
     r.HandleFunc("/api/info", endpoints.GET_info).Methods("GET")
-    r.HandleFunc("/create_account", endpoints.POST_create_account).Methods("POST")
-    r.HandleFunc("/create_device", endpoints.POST_create_device).Methods("POST")
-    r.HandleFunc("/device/{id}", endpoints.POST_device__id).Methods("POST")
-    r.HandleFunc("/device/{id}/{sensor}", endpoints.GET_device__id__sensor).Methods("GET")
-    r.HandleFunc("/devices", endpoints.GET_devices).Methods("GET")
-    r.HandleFunc("/share", endpoints.POST_share).Methods("POST")
-    r.HandleFunc("/finish_share_transaction", endpoints.POST_finish_share_transaction).Methods("POST")
-    r.HandleFunc("/login", endpoints.POST_login).Methods("POST")
-    r.HandleFunc("/logout", endpoints.GET_POST_logout)
-    r.HandleFunc("/me", endpoints.GET_me)
+    r.HandleFunc("/api/create_account", endpoints.POST_create_account).Methods("POST")
+    r.HandleFunc("/api/create_device", endpoints.POST_create_device).Methods("POST")
+    r.HandleFunc("/api/device/{id}", endpoints.POST_device__id).Methods("POST")
+    r.HandleFunc("/api/device/{id}/{sensor}", endpoints.GET_device__id__sensor).Methods("GET")
+    r.HandleFunc("/api/devices", endpoints.GET_devices).Methods("GET")
+    r.HandleFunc("/api/share", endpoints.POST_share).Methods("POST")
+    r.HandleFunc("/api/finish_share_transaction", endpoints.POST_finish_share_transaction).Methods("POST")
+    r.HandleFunc("/api/login", endpoints.POST_login).Methods("POST")
+    r.HandleFunc("/api/logout", endpoints.GET_POST_logout)
+    r.HandleFunc("/api/me", endpoints.GET_me)
     return r
 }
 
