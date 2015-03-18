651f86fa0eabaa08f6b929fa69e38c59adf28719
Greg Prisament <greg@canopy.link>
2015-03-18 15:32:46 -0700

    Implement LOD-based historic data lookup

--- src/canopy/datalayer/cassandra_datalayer/cass_vars.go
+++ src/canopy/datalayer/cassandra_datalayer/cass_vars.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Gregory Prisament
+ * Copyright 2015 Canopy Services, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,6 +41,13 @@ const (
 
     // Certain routines will return buckets of mixed sizes
     BUCKET_SIZE_MIXED,
+)
+
+type cloudVarTierEnum int
+const (
+    TIER_STANDARD cloudVarTierEnum = iota
+    TIER_ENHANCED // Extra data storage
+    TIER_ULTRA  // Even more data storage
 )
 
 // Get the bucket name based on time and bucket size.
@@ -175,9 +182,166 @@ func (device *CassDevice) InsertSample(varDef sddl.VarDef, t time.Time, value in
     }
 }
 
+func fetchAndAppendBucketSamples(apendee []cloudvar.CloudVarSample, conn CassConnection, startTime, endTime time.Time, bucketName string) error {
+    tableName := "varsample_float" // TODO: Make generic
+
+    query := device.conn.session.Query(`
+            SELECT time, value
+            FROM ` + tableName + `
+            WHERE device_id = ?
+                AND propname = ?
+                AND timprefix = ?
+                AND time >= ?
+                AND time <= ?
+    `, device.ID(), propname, bucketName, startTime, endTime).Consistency(gocql.One)
+
+    iter := query.Iter()
+    apendee := []cloudvar.CloudVarSample{}
+
+    switch datatype {
+    case sddl.DATATYPE_VOID:
+        var value interface{}
+        for iter.Scan(&timestamp) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_STRING:
+        var value string
+        for iter.Scan(&timestamp, &value) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_BOOL:
+        var value bool
+        for iter.Scan(&timestamp, &value) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_INT8:
+        var value int8
+        for iter.Scan(&timestamp, &value) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_UINT8:
+        var value uint8
+        for iter.Scan(&timestamp, &value) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_INT16:
+        var value int16
+        for iter.Scan(&timestamp, &value) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_UINT16:
+        var value uint16
+        for iter.Scan(&timestamp, &value) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_INT32:
+        var value int32
+        for iter.Scan(&timestamp, &value) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_UINT32:
+        var value uint32
+        for iter.Scan(&timestamp, &value) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_FLOAT32:
+        var value float32
+        for iter.Scan(&timestamp, &value) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_FLOAT64:
+        var value float64
+        for iter.Scan(&timestamp, &value) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_DATETIME:
+        var value time.Time
+        for iter.Scan(&timestamp, &value) {
+            apendee = append(apendee, cloudvar.CloudVarSample{timestamp, value})
+        }
+    case sddl.DATATYPE_INVALID:
+        return []cloudvar.CloudVarSample{}, fmt.Errorf("Cannot get property values for DATATYPE_INVALID");
+    default:
+        return []cloudvar.CloudVarSample{}, fmt.Errorf("Cannot get property values for datatype %d", datatype);
+    }
+
+    if err := iter.Close(); err != nil {
+        return []cloudvar.CloudVarSample{}, err
+    }
+
+    return apendee, nil
+}
+
+func (device *CassDevice) historicDataLOD(
+    varDef sddl.VarDef, 
+    startTime, 
+    endTime time.Time,
+    bucketSize bucketSizeEnum) ([]cloudvar.CloudVarSample, error) {
+
+    samples := []cloudvar.CloudVarSample{}
+
+    // Get list of all buckets containing samples we are interested in.
+    // TODO: This could happen in parallel w/ map-reduce-like algo
+    bucketNames = getBucketNamesForTimeRange(start, end time.Time, bucketSize)
+    for _, bucketName := range bucketNames {
+        err := fetchAndAppendBucketSamples(samples, device.conn, startTime, endTime, bucketName)
+        if err != nil {
+            return []cloudvar.CloudVarSample{}, err
+        }
+    }
+    return sample, nil
+}
+
+// For a given bucket size, we store several buckets of that size depending on
+// the Cloud Variable's upgrade tier.  This returns the time duration spanned
+// by the buckets of a particular size, given the upgrade tier.
+func cloudVarLODDuration(loudVarTierEnum, lod bucketSizeEnum) time.Duration {
+    type LODTier struct {
+        lod bucketSizeEnum
+        tier cloudVarTierEnum
+    }
+    return map[LODTier]time.Duration {
+        LODTier{BUCKET_SIZE_HOUR, TIER_STANDARD}: time.HOUR,
+        LODTier{BUCKET_SIZE_HOUR, TIER_ENHANCED}: 24*time.HOUR,
+        LODTier{BUCKET_SIZE_HOUR, TIER_ULTRA}: 7*24*time.HOUR,
+
+        LODTier{BUCKET_SIZE_DAY, TIER_STANDARD}: 7*time.HOUR,
+        LODTier{BUCKET_SIZE_DAY, TIER_ENHANCED}: 7*24*time.HOUR,
+        LODTier{BUCKET_SIZE_DAY, TIER_ULTRA}: 31*24*time.HOUR, // TBD
+
+        LODTier{BUCKET_SIZE_WEEK, TIER_STANDARD}: 7*24*time.HOUR,
+        LODTier{BUCKET_SIZE_WEEK, TIER_ENHANCED}: 31*24*time.HOUR, // TBD
+        LODTier{BUCKET_SIZE_WEEK, TIER_ULTRA}: 365*24*time.HOUR,
+
+        LODTier{BUCKET_SIZE_MONTH, TIER_STANDARD}: 31*24*time.HOUR, // TBD
+        LODTier{BUCKET_SIZE_MONTH, TIER_ENHANCED}: 365*24*time.HOUR,
+        LODTier{BUCKET_SIZE_MONTH, TIER_ULTRA}: 4*365*24*time.HOUR,
+
+        LODTier{BUCKET_SIZE_YEAR, TIER_STANDARD}: 365*31*24*time.HOUR,
+        LODTier{BUCKET_SIZE_YEAR, TIER_ENHANCED}: 4*365*24*time.HOUR,
+        LODTier{BUCKET_SIZE_YEAR, TIER_ULTRA}: 4*365*24*time.HOUR,
+    }
+}
+
 func (device *CassDevice) HistoricData(
     varDef sddl.VarDef, 
     startTime, 
     endTime time.Time) ([]cloudvar.CloudVarSample, error) {
 
+    // Figure out which resolution to use.
+    // Pick the lowest resolution that covers the entire requested period.
+    duration := endTime.Sub(startTime)
+    var bucketSize bucketSizeEnum
+    for bucketSize=BUCKET_SIZE_HOUR; bucketSize<LAST_BUCKET_SIZE; bucketSize++ {
+        lodDuration := cloudVarLODDuration(TIER_STANDARD, bucketSize)
+        if duration < lodDuration {
+            break;
+        }
+    }
+    if bucketSize == LAST_BUCKET_SIZE {
+        bucketSize = BUCKET_SIZE_YEAR
+    }
+
+    // Fetch the data from that LOD
+    return historicDataLOD(varDef, startTime, endTime time.Time, bucketSize)
 }
