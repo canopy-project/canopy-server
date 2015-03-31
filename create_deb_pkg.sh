9b6e28767a6a351b2ab89a7d4cc2dd828822cc65
Greg Prisament <greg@canopy.link>
2015-03-31 01:49:56 -0700

    Somewhat working deb package

--- create_deb_pkg.sh
+++ create_deb_pkg.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-rm -r _debbuild
+rm -rf _debbuild
 mkdir _debbuild
 ./create_src_pkg.sh
 cp canopy-server_15.04.orig.tar.gz _debbuild
 cd _debbuild
 tar xf canopy-server_15.04.orig.tar.gz
+rm canopy-server_15.04.orig.tar.gz
 cp -r ../debian canopy-server-15.04
+cp -r ~/.canopy/golang canopy-server-15.04
 cd canopy-server-15.04
-debuild -us -uc
+dpkg-buildpackage -us -uc
 cd ../..
