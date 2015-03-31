de2f83b8736357b46117def35c5cd5e2b4d2a0f1
Greg Prisament <greg@canopy.link>
2015-03-31 12:03:10 -0700

    Update copyrights

--- src/canopy/config/canopy_config_itf.go
+++ src/canopy/config/canopy_config_itf.go
@@ -1,4 +1,4 @@
-// Copyright 2014-2015 SimpleThings, Inc.
+// Copright 2014-2015 Canopy Services, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
