3d18b10cdd3789cbd0966f3378a2895e91700bf3
Greg Prisament <greg@canopy.link>
2015-03-31 02:20:13 -0700

    Add other files to deb package

--- create_deb_pkg.sh
+++ create_deb_pkg.sh
@@ -7,6 +7,7 @@ cd _debbuild
 tar xf canopy-server_15.04.orig.tar.gz
 rm canopy-server_15.04.orig.tar.gz
 cp -r ../debian canopy-server-15.04
+cp -r ../scripts canopy-server-15.04
 cp -r ~/.canopy/golang canopy-server-15.04
 cd canopy-server-15.04
 dpkg-buildpackage -us -uc
