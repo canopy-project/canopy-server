59f3d30f5bb738c3d78b73509446d0edf536ae82
Greg Prisament <greg@canopy.link>
2015-06-22 12:49:41 -0700

    More work on fsdb

--- src/canopy/datalayer/filesystem_datalayer/fsdb.go
+++ src/canopy/datalayer/filesystem_datalayer/fsdb.go
@@ -18,6 +18,23 @@ import os
 //
 // <path>/data/accounts/<username>
 // <path>/data/devices/<id>
+//
+// <path>/data/pigeon/workers
+//
+//      {
+//          "192.168.1.24" : true
+//      }
+//
+// <path>/data/pigeon/listeners
+//
+//      {
+//          "key0" : {
+//              "192.158.1.24" : true
+//          }
+//          "key1" : {
+//              "192.158.1.22" : true
+//          }
+//      }
 
 type FsdbDatalayer struct {
     cfg config.Config
@@ -46,6 +63,12 @@ type FsdbDevice struct {
     JsonSecretKey string `json:"secret_key"`
     JsonWSConnected bool `json:"ws_connected"`
 }
+
+type JsonWorkersObj struct {
+    JsonWorkers []string `json:workers`
+}
+
+type JsonListeners map[string][]string
 
 func (dl *FsdbDatalayer) Connect() datalayer.Connection, error {
     return &FsdbConnection{}, nil
@@ -255,20 +278,72 @@ func (device *FsdbDevice) WSConnected() bool {
     return device.JsonWSConnected
 }
 
+
 func (pigeon *PigeonSystem) GetListeners(key string) ([]string, error) {
-    // TODO: implement
+    file, err := os.Open(dl.datapath()  + "/pigeon/workers")
+    if err != nil {
+        return nil, err
+    }
+
+    // Read file and parse JSON
+    listeners := &(map[string][]string {})
+    err = json.NewDecoder(file).Decode(listeners)
+    if err != nil {
+        return nil, err
+    }
+
+    return workers.JsonWorkers, nil
+}
+
+func (pigeon *PigeonSystem) loadListeners() (map[string][]string, error) {
+    file, err := os.Open(dl.datapath()  + "/pigeon/workers")
+    if err != nil {
+        return nil, err
+    }
+
+    // Read file and parse JSON
+    workers = &FsdbWorkers{}
+    err = json.NewDecoder(file).Decode(workers)
+    if err != nil {
+        return nil, err
+    }
+}
+
+func (pigeon *PigeonSystem) saveListeners() (map[string]interface{}, error) {
+}
+
+func (pigeon *PigeonSystem) loadWorkers() (map[string]bool, error) {
 }
 
 func (pigeon *PigeonSystem) RegisterListener(hostname, key string) error {
-    // TODO: implement
+    listeners, err := pigeon.loadListeners()
 }
 
 func (pigeon *PigeonSystem) RegisterWorker(hostname string) error {
-    // TODO: implement
+    // Read
+    workers, err := pigeon.Workers()
+    if err != nil {
+        return err
+    }
+
+    // Modifiy
+    workers
 }
 
 func (pigeon *PigeonSystem) Workers() ([]string, error) {
-    // TODO: implement
+    file, err := os.Open(dl.datapath()  + "/pigeon/workers")
+    if err != nil {
+        return nil, err
+    }
+
+    // Read file and parse JSON
+    workers = &FsdbWorkers{}
+    err = json.NewDecoder(file).Decode(workers)
+    if err != nil {
+        return nil, err
+    }
+
+    return workers.JsonWorkers, nil
 }
 
 func NewDatalayer(cfg config.Config) datalayer.Datalayer {
